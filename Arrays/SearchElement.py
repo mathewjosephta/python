77e30c95ff9976ec7184266c67a99d6596b8faf4
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-07-26 01:22:49 +0530

    Create SearchElement.py

---
+++ Arrays/SearchElement.py
@@ -0,0 +1,14 @@
+# Check if element exists in list in Python
+
+a = [10, 20, 30, 40, 50]
+
+if 30 in a:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
+
+'''
+Sample output
+
+Elememt exists in the list
+'''
