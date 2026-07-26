c9327d47832ad08747c2028ae030f7ae2f602f57
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2026-07-26 09:19:12 +0530

    Create CheckSubstring.py

---
+++ Strings/CheckSubstring.py
@@ -0,0 +1,14 @@
+# Check if String Contains Substring in Python
+
+s = "Hi i am Mathew"
+
+if "Mathew" in s:
+  print("Substring Present")
+else:
+  print("Substring not present")
+
+'''
+Sample Output
+
+Substring present
+'''
